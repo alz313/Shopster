52f21a3923302c95883e3ebf44cc0f3cde8d2ff7
alz313 <lazebniy@gmail.com>
2017-09-07 17:08:47 +0300

    Added: Work checkbox on list view (saved in database)

--- app/src/main/java/ua/in/out/shopster/PurchaseListActivity.java
+++ app/src/main/java/ua/in/out/shopster/PurchaseListActivity.java
@@ -3,7 +3,6 @@ package ua.in.out.shopster;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -37,8 +36,6 @@ public class PurchaseListActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(PurchaseListActivity.this, PurchaseActivity.class);
                 startActivity(intent);
-
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
 
@@ -73,24 +70,20 @@ public class PurchaseListActivity extends AppCompatActivity {
 
     public class PurchaseListAdapter extends FirebaseRecyclerAdapter<Purchase, PurchaseHolder> implements ItemTouchHelperAdapter {
 
-        /**
-         * @param modelClass  Firebase will marshall the data at a location into an instance of a class that you provide
-         * @param modelLayout This is the layout used to represent a single item in the list. You will be responsible for populating an
-         *                    instance of the corresponding view with the data from an instance of modelClass.
-         * @param ref         The Firebase location to watch for data changes. Can also be a slice of a location, using some
-         *                    combination of {@code limit()}, {@code startAt()}, and {@code endAt()}.
-         */
         public PurchaseListAdapter(Class<Purchase> modelClass, int modelLayout, Query ref) {
             super(modelClass, modelLayout, PurchaseHolder.class, ref);
         }
 
         @Override
         protected void populateViewHolder(PurchaseHolder viewHolder, Purchase model, int position) {
+
+
             viewHolder.setName(model.getName());
-            viewHolder.setTag(getRef(position).getKey());
             viewHolder.setQty(model.getQty());
             viewHolder.setUnit(model.getUnit());
             viewHolder.setBought(model.getBought());
+            viewHolder.setDatabaseReference(getRef(position));
+
         }
 
         @Override
@@ -116,6 +109,8 @@ public class PurchaseListActivity extends AppCompatActivity {
         private final TextView mUnitField;
         private final CheckBox mBoughtField;
 
+        private DatabaseReference mDatabaseReference;
+
         public PurchaseHolder(View itemView) {
             super(itemView);
             mNameField = itemView.findViewById(R.id.tv_activity_purchase_list_item_name);
@@ -124,14 +119,25 @@ public class PurchaseListActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     Intent intent = new Intent(v.getContext(), PurchaseActivity.class);
-                    intent.putExtra(PurchaseActivity.EXTRA_PURCHASE_ID, (String) mNameField.getTag());
+                    intent.putExtra(PurchaseActivity.EXTRA_PURCHASE_ID, mDatabaseReference.getKey());
                     v.getContext().startActivity(intent);
                 }
             });
 
             mQtyField = itemView.findViewById(R.id.tv_activity_purchase_list_item_qty);
             mUnitField = itemView.findViewById(R.id.tv_activity_purchase_list_item_unit);
+
             mBoughtField = itemView.findViewById(R.id.cb_activity_purchase_list_item_bought);
+            mBoughtField.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mDatabaseReference.child("bought").setValue(((CheckBox) view).isChecked());
+                }
+            });
+        }
+
+        public void setDatabaseReference(DatabaseReference ref) {
+            mDatabaseReference = ref;
         }
 
         public void setName(String name) {
@@ -151,9 +157,6 @@ public class PurchaseListActivity extends AppCompatActivity {
             mBoughtField.setChecked(bought);
         }
 
-        void setTag(String pushId) {
-            mNameField.setTag(pushId);
-        }
 
     }
 }
