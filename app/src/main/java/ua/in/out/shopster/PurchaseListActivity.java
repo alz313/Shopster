ae15e24f23269229a3b7a1c6acc0dcd4054c8624
alz313 <lazebniy@gmail.com>
2017-09-12 20:31:53 +0300

    Navigation Drower

--- app/src/main/java/ua/in/out/shopster/PurchaseListActivity.java
+++ app/src/main/java/ua/in/out/shopster/PurchaseListActivity.java
@@ -4,11 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -30,7 +35,7 @@ import com.google.firebase.database.Query;
 import java.util.Arrays;
 
 
-public class PurchaseListActivity extends AppCompatActivity {
+public class PurchaseListActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     public static final int RC_SIGN_IN = 1;
 
     private RecyclerView mPurchasesRecyclerView;
@@ -45,7 +50,10 @@ public class PurchaseListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_purchase_list);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab_activity_purchase_list);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -55,6 +63,14 @@ public class PurchaseListActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         mPurchasesRecyclerView = findViewById(R.id.rv_activity_purchase_list_container);
 
@@ -101,11 +117,6 @@ public class PurchaseListActivity extends AppCompatActivity {
         };
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mAdapter.cleanup();
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -167,11 +178,37 @@ public class PurchaseListActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mAdapter.cleanup();
+    }
+
+
     private void onSignedInInitialize() {
     }
 
     private void onSignedOutCleanup() {
     }
+
 
     public class PurchaseListAdapter extends FirebaseRecyclerAdapter<Purchase, PurchaseHolder> implements ItemTouchHelperAdapter {
 
